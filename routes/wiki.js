91f87f9982ad322ab975842e4501f4303c07a912
David Yoon <imdavidy@gmail.com>
2017-05-01 13:08:51 -0400

    lunch break

--- routes/wiki.js
+++ routes/wiki.js
@@ -2,13 +2,14 @@ var express = require('express');
 var wikiRouter = express.Router();
 
 wikiRouter.get('/', function(req, res, next) {
-  res.send('got to GET /wiki/');
+  // res.send('got to GET /wiki/');
+  res.redirect('/');
 });
 wikiRouter.post('/', function(req, res, next) {
   res.send('got to POST /wiki/');
 });
 wikiRouter.get('/add', function(req, res, next) {
-  res.render('addpage.html')
+  res.render('addpage')
   // res.send('got to GET /wiki/add');
 });
 
